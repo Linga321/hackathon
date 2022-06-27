52c7b2351238234bf1a87ff8e6994410123b1915
Duc Thai <ducthai060501@gmail.com>
2022-06-27 18:09:11 +0300

    demo app'

--- client/utils/auth.js
+++
@@ -1,7 +0,0 @@
-/**
- * Helper function for setting JWT to
- * x-access-token header.
- * @param {string} token
- * @returns {{'x-access-token': string}} Header with JWT set.
- */
- export const setJWT = (token) => ({ 'x-access-token': token });
